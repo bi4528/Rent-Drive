709fc51bbf77061c83a8eb3fc257fee6b76cf483
Matej Kalc <mk9693@student.uni-lj.si>
2020-11-08 07:47:51 +0000

    Working on hbs for views profile, edit_profile and tuji_profile

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const ctrlUser = require("../controllers/user")
 
+/* GET users listing. */
+
+router.get('/profile', ctrlUser.profile);
+router.get('/edit_profile', ctrlUser.edit_profile);
+router.get('/tuji_profile', ctrlUser.tuji_profile);
+    
 module.exports = router;
