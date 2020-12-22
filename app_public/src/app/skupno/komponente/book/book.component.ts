ee5dfdea0166fdba4b06357590d00da0f3a6cb4c
Bojan Ilic <bi4528@student.uni-lj.si>
2020-12-23 00:49:44 +0100

    manjsi popravek

--- app_public/src/app/skupno/komponente/book/book.component.ts
+++ app_public/src/app/skupno/komponente/book/book.component.ts
@@ -23,61 +23,14 @@ export class BookComponent implements OnInit {
 
 
   ngOnInit(): void {
-    //this.getOwner();
-    //this.getVehicle();
-    //this.getRenter();
 
-    /*this.dateFrom = "2021-01-01";
-    this.dateTo = "2021-01-31";
-    this.vehicle_picture = "../../../../assets/images/chevrolet-corvette.jpg";
-<<<<<<< HEAD
-    this.pickup_locations = "nesto nesto nesto 12";*/
+    this.pickup_locations = "nesto nesto nesto 12";
     this.owner = this.bookService.owner;
     this.renter = this.bookService.renter;
     this.vehicle = this.bookService.vehicle;
     this.dateFrom = this.bookService.dateFrom.toDateString();
     this.dateTo = this.bookService.dateTo.toDateString();
     console.log(this.renter);
-    debugger;
-    /*
-=======
-    this.pickup_locations = "nesto nesto nesto 12";
-    this.owner = this.bookService.owner;
-    this.renter = this.bookService.renter;
-    this.vehicle = this.bookService.vehicle;
-    this.dateFrom = this.bookService.dateFrom.toISOString();
-    this.dateTo = this.bookService.dateTo.toISOString();
->>>>>>> 99fca84a803e02dd6bb27fd923ccc65cb004aeaf*/
   }
 
-
-  private getOwner(): void {
-    this.owner = new User();
-    this.owner._id = "1";
-    this.owner.firstname = "Pera";
-    this.owner.lastname = "Peric";
-    this.owner.username = "peca";
-    this.owner.email = "peca@gmail.com";
-    this.owner.phone_number = "123456";
-    this.owner.location = "Srbija"
-    this.owner.profile_picture = "../../../../assets/images/franka-potente-8052.jpg"
-  }
-
-  private getVehicle(): void {
-    this.vehicle = new Vehicle();
-    this.vehicle._id = "3";
-    this.vehicle.price = 24;
-  }
-
-  private getRenter(): void {
-    this.renter = new User();
-    this.renter._id = "2";
-    this.renter.firstname = "Greta";
-    this.renter.lastname = "Garbo";
-    this.renter.username = "geca";
-    this.renter.email = "geca@gmail.com";
-    this.renter.phone_number = "123456";
-    this.renter.location = "Svedska";
-    this.renter.profile_picture = "../../../../assets/images/avatarUser.png"
-  }
 }
