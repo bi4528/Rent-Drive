bbc28bba9d32b51487bbb77a5258638c9cabfcc3
marcheivanovski <mi2583@student.uni-lj.si>
2020-12-24 21:30:12 +0100

    work

--- app_api/controllers/db.js
+++ app_api/controllers/db.js
@@ -3,6 +3,30 @@ const Vehicle = mongoose.model('Vehicle');
 const User = mongoose.model('User');
 const vehiclesData = require('../models/vehicles-test.json');
 
+function Latch(limit) {
+    this.limit = limit;
+    this.count = 0;
+    this.waitBlock = function () {
+    };
+};
+
+Latch.prototype.async = function (fn, ctx) {
+    var _this = this;
+    setTimeout(function () {
+        fn.call(ctx, function () {
+            _this.count = _this.count + 1;
+            if (_this.limit <= _this.count) {
+                _this.waitBlock.call(_this.waitBlockCtx);
+            }
+        });
+    }, 0);
+};
+
+Latch.prototype.await = function (callback, ctx) {
+    this.waitBlock = callback;
+    this.waitBlockCtx = ctx;
+};
+
 const addSampleData = (req, res) => {
     TODO
 };
