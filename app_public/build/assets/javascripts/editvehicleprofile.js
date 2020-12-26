b3ac2b39c64e6b9ab8ba3e33f248fc9f57c96d8c
KalcMatej99 <mk9693@student.uni-lj.si>
2020-12-26 18:54:44 +0100

    Added swagger for users

--- app_public/build/assets/javascripts/editvehicleprofile.js
+++ app_public/build/assets/javascripts/editvehicleprofile.js
@@ -156,7 +156,6 @@ document.getElementById("save-changes").addEventListener("click", function (dogo
         errors += "You must insert a description.\n";
         writeError = true;
     }
-    //debugger;
     if (!validate_dates(date1.value, date2.value)) {
         date1.classList.add("alert-danger");
         date2.classList.add("alert-danger");
